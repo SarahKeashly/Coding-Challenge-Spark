8d878910575395eacb9b412f4b0525f6cdd1ee05
Sarah Keashly <sarah.keashly@makedigital.ca>
2022-03-13 17:05:58 -0700

    Figure out how to print out table rows. Need to loop through rows.  Branch off here to see how to do this

--- csvImport.rb
+++ csvImport.rb
@@ -5,10 +5,19 @@ require 'csv'
 # table_parse = CSV.parse("contacts.csv")
 
 
+# 1. Import the contacts CSV (included). 
+# to create a database.
 # prints out parsed table
 table = CSV.parse(File.read("contacts.csv"), headers: true)
 # puts "parsed Table", table;
 
 
-puts table[0]["first_name"]
+#2. List only the valid contact records (not duplicate or incomplete), with each contact appearing only once along with the answers they have most recently added (if they have any answers at all).
+
+# puts table[0]["first_name"]
 # "Glenda"
+
+# puts table[1]
+# lists eveything for Jennifyr
+
+# Need to loop through the rows
